da7da7de1eb2a7e009f10e994a8ce747d500f1ac
SilvinPradhan <silvinpradhan95@gmail.com>
2020-07-18 13:49:26 -0500

    created the background design for the page and commenced the states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
+
+mic.continuous = true;
+mic.interimResults = true;
+mic.lang = 'en-US';
+
 function App() {
+
+const [isListening, setIsListening] = useState('false');
+const [note, setNote] = useState(null);
+const [savedNotes, setSavedNotes] = useState([]);
+
+const handleListen = () => {
+  if(isListening){
+    mic.start()
+    mic.onend = () => {
+      console.log('continue..')
+      mic.start()
+    } 
+  }else{
+    mic.stop()
+    mic.onend = () => {
+      console.log('Stopped Mic on Click')
+    }
+  }
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>Voice Notes</h1>
+    <div className="container">
+      <div className="box">
+        <h2>Current Note</h2>
+        {isListening ? <span role="img">🎙</span> : <span role="img">🎙</span>}
+        <button onClick={handleSaveNote} disabled={!note}>Save Note</button>
+        <button onClick = {() => setIsListening(prevState => !prevState)}> Start / Stop</button>
+        <p>{note}</p>
+      </div>
+      <div className='box'>
+        <h2>Notes</h2>
+        {savedNotes.map(n => (
+          <p key={n}>{n}</p>
+        ))}
+      </div>
     </div>
+    </>
   );
 }
 
